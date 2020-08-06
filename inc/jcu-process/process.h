8900b7935163a43209140957317c459c089b7a34
Jichan <development@jc-lab.net>
2020-08-06 17:17:53 +0900

    first commit

---
+++ inc/jcu-process/process.h
@@ -0,0 +1,62 @@
+/**
+ * @file	process.h
+ * @author	Joseph Lee <development@jc-lab.net>
+ * @date	2020/08/06
+ * @copyright Copyright (C) 2020 jc-lab.\n
+ *            This software may be modified and distributed under the terms
+ *            of the Apache License 2.0.  See the LICENSE file for details.
+ */
+
+#ifndef JCU_PROCESS_SRC_PROCESS_H_
+#define JCU_PROCESS_SRC_PROCESS_H_
+
+#include <functional>
+
+namespace jcu {
+namespace process {
+
+enum EventType {
+  kProcessExited = 0x0001,
+  kReadStdout = 0x0010,
+  kReadStderr = 0x0020,
+};
+
+enum EventLoopResult {
+  kEventLoopError = -1,
+  kEventLoopDone = 0,
+  kEventLoopIdle = 1,
+  kEventLoopHandled = 2,
+};
+
+typedef std::function<void(EventType event_type, const char* buffer, size_t length)> EventLoopHandler_t;
+
+class Process {
+ public:
+  virtual int execute() = 0;
+
+  /**
+   *
+   * @return
+   * - -1 : Error
+   * - 0 : Done
+   * - 1 : Running... (Maybe need throttling)
+   * - 2 : Event Handled...
+   */
+  virtual EventLoopResult eventloop(const EventLoopHandler_t& handler, int *perr = nullptr) = 0;
+
+  virtual bool isAlive() const = 0;
+  virtual bool checkAlive() = 0;
+
+  virtual int terminateProcess() = 0;
+
+ public:
+  static Process* prepare(const char *command);
+#ifdef _UNICODE
+  static Process* prepare_w(const wchar_t *command);
+#endif
+};
+
+} // namespace process
+} // namespace jcu
+
+#endif //JCU_PROCESS_SRC_PROCESS_H_
---
+++ src/windows/process.cc
@@ -0,0 +1,281 @@
+/**
+ * @file	process.cc
+ * @author	Joseph Lee <development@jc-lab.net>
+ * @date	2020/08/06
+ * @copyright Copyright (C) 2020 jc-lab.\n
+ *            This software may be modified and distributed under the terms
+ *            of the Apache License 2.0.  See the LICENSE file for details.
+ */
+
+#include <string>
+#include <vector>
+#include <codecvt>
+
+#include <string.h>
+#include <tchar.h>
+#include <windows.h>
+
+#include <jcu-process/process.h>
+
+namespace jcu {
+namespace process {
+
+namespace windows {
+
+static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>,wchar_t> s_utf8_to_wstr_convert;
+
+class PipePair {
+ private:
+  HANDLE wr_;
+  HANDLE rd_;
+  DWORD err_;
+
+ public:
+  ~PipePair() {
+    close();
+  }
+
+  PipePair()
+  : wr_(nullptr), rd_(nullptr)
+  {
+    if (!::CreatePipe(&rd_, &wr_, nullptr, 0)) {
+      err_ = (int)::GetLastError();
+      return;
+    }
+    err_ = 0;
+  }
+
+  DWORD getError() const {
+    return err_;
+  }
+
+  void close() {
+    if (wr_ && wr_ != INVALID_HANDLE_VALUE) {
+      ::CloseHandle(wr_);
+      wr_ = nullptr;
+    }
+    if (rd_ && rd_ != INVALID_HANDLE_VALUE) {
+      ::CloseHandle(rd_);
+      rd_ = nullptr;
+    }
+  }
+  HANDLE getWriteHandle() {
+    return wr_;
+  }
+  HANDLE getReadHandle() {
+    return rd_;
+  }
+  HANDLE detachWriteHandle() {
+    HANDLE h = wr_;
+    wr_ = nullptr;
+    return h;
+  }
+  HANDLE detachReadHandle() {
+    HANDLE h = rd_;
+    wr_ = nullptr;
+    return h;
+  }
+  HANDLE detachInheritableWriteHandle() {
+    HANDLE handle = nullptr;
+    if (!DuplicateHandle(
+        GetCurrentProcess(), wr_,
+        GetCurrentProcess(), &handle, 0,
+        TRUE, DUPLICATE_SAME_ACCESS)) {
+      err_ = ::GetLastError();
+      return nullptr;
+    }
+    wr_ = nullptr;
+    return handle;
+  }
+  HANDLE detachInheritableReadHandle() {
+    HANDLE handle = nullptr;
+    if (!DuplicateHandle(
+        GetCurrentProcess(), rd_,
+        GetCurrentProcess(), &handle, 0,
+        TRUE, DUPLICATE_SAME_ACCESS)) {
+      err_ = ::GetLastError();
+      return nullptr;
+    }
+    rd_ = nullptr;
+    return handle;
+  }
+};
+
+class WindowsProcess : public Process {
+ private:
+  std::basic_string<wchar_t> command_line_;
+  PROCESS_INFORMATION pi_;
+  PipePair pipe_stdin_;
+  PipePair pipe_stdout_;
+  PipePair pipe_stderr_;
+
+  bool process_alive_;
+
+  WindowsProcess(const std::basic_string<wchar_t> &command_line)
+  : command_line_(command_line), pi_({0}), process_alive_(false)
+  { }
+
+ public:
+  virtual ~WindowsProcess() {
+
+  }
+
+  int execute() override {
+    int err_code;
+
+    std::vector<wchar_t> command_line_buf(command_line_.length() + 1);
+    ::memcpy(command_line_buf.data(), command_line_.c_str(), command_line_.size() * sizeof(wchar_t));
+    command_line_buf[command_line_.length()] = 0;
+
+    STARTUPINFOW si = {0};
+    ::memset(&si, 0, sizeof(si));
+    si.cb = sizeof(si);
+    si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
+    si.wShowWindow = SW_HIDE;
+
+    err_code = pipe_stdin_.getError();
+    if (err_code) return err_code;
+    err_code = pipe_stdout_.getError();
+    if (err_code) return err_code;
+    err_code = pipe_stderr_.getError();
+    if (err_code) return err_code;
+
+    si.hStdInput = pipe_stdin_.detachInheritableReadHandle();
+    si.hStdOutput = pipe_stdout_.detachInheritableWriteHandle();
+    si.hStdError = pipe_stderr_.detachInheritableWriteHandle();
+
+    if(!::CreateProcessW(
+        nullptr,
+        command_line_buf.data(),
+        nullptr,
+        nullptr,
+        TRUE,
+        CREATE_NEW_CONSOLE,
+        nullptr,
+        nullptr,
+        &si,
+        &pi_
+        )) {
+      if (si.hStdInput) {
+        ::CloseHandle(si.hStdInput);
+      }
+      if (si.hStdOutput) {
+        ::CloseHandle(si.hStdOutput);
+      }
+      if (si.hStdError) {
+        ::CloseHandle(si.hStdError);
+      }
+      return ::GetLastError();
+    }
+
+    process_alive_ = true;
+    return 0;
+  }
+
+  EventLoopResult eventloop(const EventLoopHandler_t &handler, int *perr) override {
+    HANDLE handles[3] = {
+        pipe_stdout_.getReadHandle(), pipe_stderr_.getReadHandle(), pi_.hProcess
+    };
+    char buffer[128] = {0};
+    int processed_size = 0;
+    DWORD dwWait = ::WaitForMultipleObjects(3, handles, FALSE, 100);
+    if (dwWait == WAIT_TIMEOUT) {
+      return kEventLoopIdle;
+    }else if((dwWait < 0) || (dwWait > (WAIT_OBJECT_0 + 2))) {
+      if (perr) *perr = ::GetLastError();
+      return kEventLoopError;
+    }
+
+    int handle_index = dwWait - WAIT_OBJECT_0;
+    HANDLE target_handle = handles[handle_index];
+    DWORD pipe_read_bytes = 0;
+    DWORD pipe_total_bytes_avail = 0;
+    DWORD pipe_bytes_left_this_message = 0;
+    if ((handle_index == 0) || (handle_index == 1)) {
+      if (::PeekNamedPipe(target_handle, buffer, sizeof(buffer), &pipe_read_bytes, &pipe_total_bytes_avail, &pipe_bytes_left_this_message)) {
+        printf("*peek pipe = %d / %d / %d\n", pipe_read_bytes, pipe_total_bytes_avail, pipe_bytes_left_this_message);
+        if (pipe_read_bytes > 0) {
+          if (::ReadFile(target_handle, buffer, sizeof(buffer), &pipe_read_bytes, nullptr)) {
+            processed_size = pipe_read_bytes;
+            switch (dwWait) {
+              case (WAIT_OBJECT_0):
+                handler(kReadStdout, buffer, processed_size);
+                break;
+              case (WAIT_OBJECT_0 + 1):
+                handler(kReadStderr, buffer, processed_size);
+                break;
+            }
+            return kEventLoopHandled;
+          }
+        }
+      }
+    }
+
+    if (handle_index < 2) {
+      dwWait = ::WaitForSingleObject(pi_.hProcess, 100);
+      if (dwWait == WAIT_OBJECT_0) {
+        dwWait = (WAIT_OBJECT_0 + 2);
+      }
+    }
+
+    switch (dwWait) {
+      case (WAIT_OBJECT_0 + 2):
+        process_alive_ = false;
+        handler(kProcessExited, buffer, processed_size);
+        return kEventLoopDone;
+    }
+    return kEventLoopIdle;
+  }
+
+  bool isAlive() const override {
+    if (!process_alive_) {
+      return false;
+    }
+
+    DWORD dwWait = ::WaitForSingleObject(pi_.hProcess, 0);
+    if (dwWait == WAIT_OBJECT_0) {
+      return false;
+    }
+
+    return true;
+  }
+
+  bool checkAlive() override {
+    if (!process_alive_) {
+      return false;
+    }
+    bool alive = isAlive();
+    process_alive_ = alive;
+    return alive;
+  }
+
+  int terminateProcess() override {
+    if (!::TerminateProcess(pi_.hProcess, 1)) {
+      return (int)::GetLastError();
+    }
+    return 0;
+  }
+
+ public:
+  static Process *prepare(const std::basic_string<wchar_t>& command_line) {
+    return new WindowsProcess(command_line);
+  }
+};
+
+}
+
+Process* Process::prepare(const char *command) {
+  return windows::WindowsProcess::prepare(
+      windows::s_utf8_to_wstr_convert.from_bytes(command)
+      );
+}
+
+#ifdef _UNICODE
+Process* Process::prepare_w(const wchar_t *command) {
+  return windows::WindowsProcess::prepare(command);
+}
+#endif
+
+
+} // namespace process
+} // namespace jcu
